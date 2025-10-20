24f29f2a9561bfbe49144e0aeaa7b713150ecd07
wheeler_k <kenton.wheeler@solent.ac.uk>
2025-10-20 10:22:27 +0100

    Version 1.2

--- week1.py
+++ week1.py
@@ -1,4 +1,7 @@
 name = input("Please enter your name")
 age = int(input("Enter your current age"))
+age = age + 1
 age += 1
+age -= 1
 print(f"On your next birthday {name}, you will be {age} years old")
+print("On your next birthday", name, "you will be", age, "years old")
