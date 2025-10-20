8563131ef4fc02ca55d76d2ab54d81f2dbf7b06b
wheeler_k <kenton.wheeler@solent.ac.uk>
2025-10-20 09:38:49 +0100

    Version 1.1

--- week1.py
+++ week1.py
@@ -1,5 +1,4 @@
 name = input("Please enter your name")
 age = int(input("Enter your current age"))
-print(f"Hi {name}. Welcome to Solent")
 age += 1
-print(f"On your next birthday, you will be {age} years old")
+print(f"On your next birthday {name}, you will be {age} years old")
